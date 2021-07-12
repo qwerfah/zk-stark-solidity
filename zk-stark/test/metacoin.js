525875dfc7f021e3e1e2981d25295db75bc9e6b0
qwerfah <qwerfah@gmail.com>
2021-07-12 23:31:56 +0300

    Tests updated

--- zk-stark/test/metacoin.js
+++ zk-stark/test/metacoin.js
@@ -1,37 +1,49 @@
 const MetaCoin = artifacts.require("MetaCoin");
+const truffleAssert = require('truffle-assertions');
 
 contract('MetaCoin', (accounts) => {
+  let instance;
+  beforeEach('should setup the contract instance', async () => {
+    instance = await MetaCoin.deployed();
+  });
+
   it('should put 10000 MetaCoin in the first account', async () => {
-    const metaCoinInstance = await MetaCoin.deployed();
-    const balance = await metaCoinInstance.getBalance.call(accounts[0]);
+    const balance = await instance.getBalance.call(accounts[0]);
 
     assert.equal(balance.valueOf(), 10000, "10000 wasn't in the first account");
   });
+
   it('should call a function that depends on a linked library', async () => {
-    const metaCoinInstance = await MetaCoin.deployed();
-    const metaCoinBalance = (await metaCoinInstance.getBalance.call(accounts[0])).toNumber();
-    const metaCoinEthBalance = (await metaCoinInstance.getBalanceInEth.call(accounts[0])).toNumber();
+    const metaCoinBalance = (await instance.getBalance.call(accounts[0])).toNumber();
+    const metaCoinEthBalance = (await instance.getBalanceInEth.call(accounts[0])).toNumber();
 
     assert.equal(metaCoinEthBalance, 2 * metaCoinBalance, 'Library function returned unexpected function, linkage may be broken');
   });
+  
   it('should send coin correctly', async () => {
-    const metaCoinInstance = await MetaCoin.deployed();
-
     // Setup 2 accounts.
+    // accounts[0] = 0;
     const accountOne = accounts[0];
     const accountTwo = accounts[1];
 
     // Get initial balances of first and second account.
-    const accountOneStartingBalance = (await metaCoinInstance.getBalance.call(accountOne)).toNumber();
-    const accountTwoStartingBalance = (await metaCoinInstance.getBalance.call(accountTwo)).toNumber();
+    const accountOneStartingBalance = (await instance.getBalance.call(accountOne)).toNumber();
+    const accountTwoStartingBalance = (await instance.getBalance.call(accountTwo)).toNumber();
 
     // Make transaction from first account to second.
     const amount = 10;
-    await metaCoinInstance.sendCoin(accountTwo, amount, { from: accountOne });
-
+    const result = await instance.sendCoin(accountTwo, amount, { from: accountOne });
+    
+    // Check if event was emitted and it has correct arguments
+    truffleAssert.eventEmitted(result, 'Transfer', (event) =>{
+      return event._from == accountOne 
+          && event._to == accountTwo 
+          && event._value == amount;
+    });
+;
     // Get balances of first and second account after the transactions.
-    const accountOneEndingBalance = (await metaCoinInstance.getBalance.call(accountOne)).toNumber();
-    const accountTwoEndingBalance = (await metaCoinInstance.getBalance.call(accountTwo)).toNumber();
+    const accountOneEndingBalance = (await instance.getBalance.call(accountOne)).toNumber();
+    const accountTwoEndingBalance = (await instance.getBalance.call(accountTwo)).toNumber();
 
 
     assert.equal(accountOneEndingBalance, accountOneStartingBalance - amount, "Amount wasn't correctly taken from the sender");
