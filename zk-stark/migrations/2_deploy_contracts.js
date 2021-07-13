7bf95b93c14acab4abc5ce91e3d6a2493e6a7594
qwerfah <qwerfah@gmail.com>
2021-07-13 23:31:30 +0300

    Migrations updated

--- zk-stark/migrations/2_deploy_contracts.js
+++ zk-stark/migrations/2_deploy_contracts.js
@@ -1,8 +1,10 @@
 const ConvertLib = artifacts.require("ConvertLib");
 const MetaCoin = artifacts.require("MetaCoin");
+const Element = artifacts.require("Element");
 
 module.exports = function(deployer) {
   deployer.deploy(ConvertLib);
   deployer.link(ConvertLib, MetaCoin);
   deployer.deploy(MetaCoin);
+  deployer.deploy(Element);
 };
