e1e29595f21fddf6d0ad6f814eca1638d1211df0
qwerfah <qwerfah@gmail.com>
2021-07-14 16:14:40 +0300

    Carry-less multiplication rewritten in Yul

--- zk-stark/contracts/fft/Element.sol
+++ zk-stark/contracts/fft/Element.sol
@@ -13,13 +13,14 @@ contract Element {
 
     // Get specified bit from given 128-bit integer
     function getBit(uint128 num, uint8 n)
-        internal
+        public
         pure
         restrictSize(n)
-        returns (uint8)
+        returns (uint8 res)
     {
-        uint128 shifted = 1 * uint128(2)**n;
-        return num & shifted > 0 ? 1 : 0;
+        assembly {
+            res := gt(and(num, exp(2, n)), 0)
+        }
     }
 
     // Set specified bit in given 128-bit integer
@@ -27,40 +28,105 @@ contract Element {
         uint128 num,
         uint8 n,
         bool bit
-    ) internal pure restrictSize(n) returns (uint128) {
-        uint128 shifted = 1 * uint128(2)**n;
-
-        return bit ? num | shifted : num & ~shifted;
+    ) public pure restrictSize(n) returns (uint128 res) {
+        assembly {
+            switch bit
+                case 0 {
+                    res := and(num, not(exp(2, n)))
+                }
+                case 1 {
+                    res := or(num, exp(2, n))
+                }
+        }
     }
 
     // Carryless xor of a and b
-    function clMulXor(uint128 a, uint128 b) public pure returns (uint128) {
+    function clMulXor(uint128 a, uint128 b) public pure returns (uint128 res) {
+        assembly {
+            function getBit(num, n) -> result {
+                result := gt(and(num, exp(2, n)), 0)
+            }
+
+            function setBit(num, n, bit) -> result {
+                result := and(num, not(exp(2, n)))
+            }
+
+            res := setBit(0, 0, mul(getBit(a, 0), getBit(b, 0)))
+            let xor1
+
+            for {
+                let i := 0
+            } lt(i, 64) {
+                i := add(i, 1)
+            } {
+                xor1 := mul(getBit(a, 0), getBit(b, i))
+
+                for {
+                    let j := 1
+                } or(lt(j, i), eq(j, i)) {
+                    j := add(j, 1)
+                } {
+                    if mul(getBit(a, j), getBit(b, sub(i, j))) {
+                        xor1 := xor(xor1, 1)
+                    }
+                }
+
+                if xor1 {
+                    res := setBit(res, i, xor1)
+                }
+            }
+
+            for {
+                let i := 64
+            } lt(i, 127) {
+                i := add(i, 1)
+            } {
+                xor1 := mul(getBit(a, sub(i, 63)), getBit(b, 63))
+
+                for {
+                    let j := sub(i, 62)
+                } lt(j, 64) {
+                    j := add(j, 1)
+                } {
+                    if mul(getBit(a, j), getBit(b, sub(i, j))) {
+                        xor1 := xor(xor1, 1)
+                    }
+                }
+
+                if xor1 {
+                    res := setBit(res, i, xor1)
+                }
+            }
+        }
+
+        /*
         uint128 res = setBit(0, 0, getBit(a, 0) * getBit(b, 0) == 1);
         uint8 i = 1;
 
         for (; i < 64; i++) {
-            uint8 xor = getBit(a, 0) * getBit(b, i);
-            uint8 j = 1;
+            uint8 xor1 = getBit(a, 0) * getBit(b, i);
+            uint8 j1 = 1;
 
-            for (; j <= i; j++) {
-                xor ^= (getBit(a, j) * getBit(b, i - j));
+            for (; j1 <= i; j1++) {
+                xor1 ^= (getBit(a, j1) * getBit(b, i - j1));
             }
 
-            res = setBit(res, i, xor == 1);
+            res = setBit(res, i, xor1 == 1);
         }
 
         for (; i < 127; i++) {
-            uint8 xor = getBit(a, i - 63) * getBit(b, 63);
-            uint8 j = i - 62;
+            uint8 xor2 = getBit(a, i - 63) * getBit(b, 63);
+            uint8 j2 = i - 62;
 
-            for (; j < 64; j++) {
-                xor ^= (getBit(a, j) * getBit(b, i - j));
+            for (; j2 < 64; j2++) {
+                xor2 ^= (getBit(a, j2) * getBit(b, i - j2));
             }
 
-            res = setBit(res, i, xor == 1);
+            res = setBit(res, i, xor2 == 1);
         }
-
+        
         return res;
+        */
     }
 
     //
